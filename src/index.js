74a0212d309e38bcecd08b75afcdec4f8b8e3ed6
Arooj448 <aroojali448@gmail.com>
2024-12-21 20:31:42 +0000

    react-router

--- src/index.js
+++ src/index.js
@@ -1,13 +1,117 @@
-import React from 'react';
+import React, {useEffect , useState} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
+import { BrowserRouter ,Routes , Route, useParams , NavLink} from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const Home =()=>{
+  const [posts , setPosts] =useState([]);
+
+useEffect (() =>{       {/* way to structure useeffect*/}
+ fetch('https://jsonplaceholder.typicode.com/posts')
+ .then((data => data.json()))
+ .then((data=> setPosts(data)));
+},[]);
+
+
+  return(
+    <div>
+      <div className='post-container'>
+        {posts.map((post)=>(
+          <NavLink style={{ display: 'block' }} to={`/post/${post.id}`}>
+          {post.title}
+        </NavLink>
+        
+        ))}
+      </div>
+    </div>
+   
+
+  );
+};
+
+const About =()=>{
+  return(
+    <h1>Company BIO </h1>
+  );
+};
+
+const Profile =()=>{
+  return (
+    <h1>Candidate Profile</h1>
+  )
+};
+
+const SayUser =()=>{
+  const params = useParams();
+  // console.log ("params", params);   {/* dynamic routing url make by id*/}
+  return(                    
+    <div>
+      <h1>My Nmae is {params.userId}</h1>  
+    </div>
+
+    
+  );
+  
+};
+
+const PostPage = ()=>{
+  const params = useParams();
+  const [data, setData] =useState(null)
+ 
+  useEffect (()=>{
+    fetch(`https://jsonplaceholder.typicode.com/posts/${params.postId}`)
+    .then
+    (data =>data.json())
+    .then(data =>setData(data));
+
+  },[]);
+
+  console.log("Data" , data)
+
+if( data === null)
+  return <p>Loading.. </p>;
+return(
+
+  <div>
+    <h1>
+      {data.title}
+      <p>{data.body}</p>
+    </h1>
+  </div>
+);
+
+
+  
+}
+
+
+
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+
+
+      <Route path='/account/about' element={<About/>}/>  {/*Multi level Routing*/}
+      <Route path='/user/:userId' element={<SayUser/>}/>
+      <Route path='/post/:postId' element={<PostPage/>}/>
+      {/*Nested Routing   for now all routes are hardcoded*/}
+
+      <Route path='/account'>
+        <Route path='profile' element={<Profile/>}/>
+      </Route>
+    
+
+    </Routes>
+    
+    </BrowserRouter>
   </React.StrictMode>
 );
 
